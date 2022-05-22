4dfd9ae81432762f25b612dcafa8b16032257614
Arsh Batth <10015486@sbstudents.org>
2022-05-22 16:06:36 -0400

    data structures

---
+++ SuperList-2/SuperList.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class SuperList<E>
+{
+	private ListNode<E> root;
+	private ListNode<E> end;
+	private int size;
+
+	public SuperList()
+	{
+		root = null;
+		end = null;
+		size = 0;
+	}
+
+	public void add(E val)
+	{
+
+	}
+}
