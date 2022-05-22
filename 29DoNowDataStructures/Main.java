111513d198e04fa77ca3c6714aa2e2e871c66b11
Arsh Batth <10015486@sbstudents.org>
2022-05-22 01:55:40 -0400

    data structures

---
+++ 29DoNowDataStructures/Main.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+import java.util.stream.*;
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Map<String, int[]> linReg = new HashMap<String, int[]>();
+    List<Double> x = new ArrayList<Double>();
+    List<Double> y = new ArrayList<Double>();
+
+    //x is net points and y is wins
+
+    try
+    {
+    BufferedReader input = new BufferedReader(new FileReader("TeamScores.csv"));
+    //input.readLine();
+    String text;
+
+    while((text = input.readLine()) != null)
+    {
+    String[] data = text.split(",");
+    linReg.put(data[0], new int[]{Integer.parseInt(data[1]), Integer.parseInt(data[2])});
+    x.add(Double.parseDouble(data[1]));
+    y.add(Double.parseDouble(data[2]));
+    }
+
+    for(Map.Entry<String,int[]> entry: linReg.entrySet())
+    {
+    System.out.println("Name: " + entry.getKey() +  " Wins: " + entry.getValue()[0] + " Net Points: " + entry.getValue()[1]);
+    }
+    }
+    catch(Exception E)
+    {
+    System.out.println("File not found");
+    }
+
+    //Converts arraylist to Double[] array (Object)
+    Double[] x1 = new Double[x.size()];
+    Double[] y1 = new Double[y.size()];
+
+    x1 = x.toArray(x1);
+    y1 = y.toArray(y1);
+
+    //Had to convert object Double[] to primitive type double[]
+    double[] x2 = Stream.of(x1).mapToDouble(Double::doubleValue).toArray();
+    double[] y2 = Stream.of(y1).mapToDouble(Double::doubleValue).toArray();
+
+    System.out.println(x2);
+    System.out.println(y2);
+    
+    LinearRegression equation = new LinearRegression(y2,x2);
+
+    System.out.println(equation);
+
+    boolean askOn = true;
+
+    while(askOn)
+    {
+      //Lookup Tool
+      Scanner myObj = new Scanner(System.in);
+      System.out.println("Enter Team: ");
+      String teamName = myObj.nextLine();
+      if(teamName.equalsIgnoreCase("No"))
+        askOn = false;
+      //Actual Wins
+      if(askOn)
+      {
+        int[] actualWins = linReg.get(teamName);
+        System.out.println(actualWins[0]);
+        //Predicted Wins
+        double netPoints = actualWins[0];
+        double predictedWins = equation.predict(netPoints);
+        System.out.println(predictedWins);
+      }
+    }
+  }
+}
